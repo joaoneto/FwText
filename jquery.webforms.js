1ad478890832f1f3af2c66cbe376126d7c5d9aa4
João Neto <joaopintoneto@gmail.com>
2013-01-13 15:25:44 -0200

    Implemented upload and added submodule Font-Awesome

--- jquery.webforms.js
+++ jquery.webforms.js
@@ -3,35 +3,60 @@
  * Copyright (c) 2013 João Pinto Neto
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  */
-(function($){
+(function ($) {
+
   function log() {
-    if ($.fn.wf_debug && console && console.log)
-      console.log.apply(this, arguments);
+    // if ($.fn.wf_debug && console && console.log) {
+    //   console.log.apply(this, arguments);
+    // }
   }
 
-  $.fn.richtext = function(options) {
+  $.fn.richtext = function (options) {
     var instance, textarea;
     var settings = $.extend(
+      {},
       {
         'toolbar': { 'buttons': [
-            'font',
-            'bold',
-            'italic',
-            'link',
-            'picture',
-            'list',
-            'indent-left',
-            'indent-right',
-            'align-left',
-            'align-center',
-            'align-right'
+          'font',
+          'bold',
+          'italic',
+          'link',
+          'picture',
+          'list',
+          'indent-left',
+          'indent-right',
+          'align-left',
+          'align-center',
+          'align-right'
         ] }
       },
       options
     );
-    
+
+    function exec(type, arg) {
+      arg = arg || null;
+
+      if (type === 'list') {
+        type = 'insertUnorderedList';
+      } else if (type === 'align-left') {
+        type = 'JustifyLeft';
+      } else if (type === 'align-center') {
+        type = 'JustifyCenter';
+      } else if (type === 'align-right') {
+        type = 'JustifyRight';
+      } else if (type === 'indent-right') {
+        type = 'Outdent';
+      } else if (type === 'indent-left') {
+        type = 'Indent';
+      } else if (type === 'picture') {
+        type = 'insertImage';
+      }
+
+      return this.document.execCommand(type, false, arg);
+    };
+
     function generateRichTextArea() {
-        textarea.richTextArea = $('<div/>')
+      textarea.richTextArea = $('<div/>')
         .css({
           width: textarea.width(),
           height: textarea.height()
@@ -40,63 +65,25 @@
         .appendTo(textarea.parent())
         .attr('contentEditable', true);
     }
-    
+
     function generateToolbar() {
-        textarea.toolBar = $('<div/>')
+      textarea.toolBar = $('<div/>')
         .addClass('wf-richtext-toolbar')
         .appendTo(textarea.parent());
 
-        $(settings.toolbar.buttons).each(function(i, btn) {
-            var img = btn != 'link' ? btn.toLowerCase() : 'share';
-            var btnEl = $('<a/>')
-              .addClass('btn')
-              .attr('href', '#')
-              .append($('<i/>').addClass('icon-' + img))
-              .click(function(e) {
-                e.preventDefault();
-                exec(btn);
-                textarea.richTextArea.focus();
-              })
-            textarea.toolBar.append(btnEl);
-        });
-    }
-
-    function exec(type, arg) {
-      arg = arg || null;
-      if (type == 'list') {
-        type = 'insertUnorderedList';
-      }
-      else if (type == 'align-left') {
-        type = 'JustifyLeft';
-      }
-      else if (type == 'align-center') {
-        type = 'JustifyCenter';
-      }
-      else if (type == 'align-right') {
-        type = 'JustifyRight';
-      }
-      else if (type == 'indent-right') {
-        type = 'Outdent';
-      }
-      else if (type == 'indent-left') {
-        type = 'Indent';
-      }
-      else if (type == 'picture') {
-        return uploadFile();
-      }
-      
-      return this.document.execCommand(type, false, arg);
-    }
-
-    function uploadFile() {
-      // <form action="upload.php" method="post" enctype="multipart/form-data" class="img-upload form-horizontal">
-      // <input type="file" name="img_file" class="input-file" />
-
-      // var $form = $('<form/>')
-      // .attr({action: 'upload.php', method: 'post', enctype: 'multipart/form-data'})
-      // .imgupload(function(data){log(data)})
-      // .append($(''))
-      // log($form)
+      $(settings.toolbar.buttons).each(function (i, btn) {
+        var img = btn !== 'link' ? btn.toLowerCase() : 'share';
+        var btnEl = $('<a/>')
+          .addClass('btn')
+          .attr('href', '#')
+          .append($('<i/>').addClass('icon-' + img))
+          .on('click', function (e) {
+            e.preventDefault();
+            exec(btn);
+            textarea.richTextArea.focus();
+          });
+        textarea.toolBar.append(btnEl);
+      });
     }
 
     function init() {
@@ -107,9 +94,15 @@
       return this;
     }
 
-    return this.each(function() {
+    this.test = function () {
+      log('test');
+      return this;
+    };
+
+    return this.each(function () {
       if (!$(this).data('instance')) {
         $(this).data('instance', init.apply(this));
+        log('richtext instancied!');
       }
       instance = $(this).data('instance');
     });
@@ -118,52 +111,48 @@
 
 
 
-  $.fn.imgupload = function(options, callback) {
-    callback = typeof options == 'function' ? options : callback || function() {};
-    options = typeof options != 'function' ? options : {};
+  $.fn.imgupload = function (options, callback) {
+    callback = callback || function () {};
+    options = options || {};
 
     var instance, img_form;
-    var settings = $.extend(
-      { 'uploadDir': 'upload' },
+    var params = $.extend(
+      {},
+      $.ajaxSettings,
+      { contentType: false, processData: false, cache: false, type: 'post' },
+      { 'uploadDir': '.' },
       options
     );
 
     function init() {
       img_form = $(this);
-      img_form.submit(function() {
-        var form_data = new FormData(img_form[0]);
-        log(form_data);
-
-        var params = $.extend(true, {}, $.ajaxSettings, {
-          contentType: false,
-          processData: false,
-          cache: false,
-          type: method || 'POST'
-        });
-
+      img_form.submit(function (e) {
+        e.preventDefault();
+        params.url = options.url || $(img_form).attr('action') || params.url;
+        params.type = options.type || $(img_form).attr('method') || params.type;
+        params.data = new FormData(this);
+        params.data.append('uploadDir', params.uploadDir);
+        params.success = function (data) {
+          log(data)
+          if (data.files) {
+            $(data.files).each(function (i, p) {
+              // data.files
+              $(this).siblings('.richtext').exec('picture', p);
+            });
+          }
+        };
         $.ajax(params);
-
-        // $(this).ajaxSubmit({
-        //   dataType:  'json',
-        //   // beforeSubmit:  function() {
-        //   // },
-        //   success: function(responseText, statusText, xhr, $form) {
-        //     callback(responseText);
-        //   },
-        //   clearForm: true
-        // });
-        return false;
       });
-
       return this;
     }
 
-    return this.each(function() {
+    return this.each(function () {
       if (!$(this).data('instance')) {
         $(this).data('instance', init.apply(this));
+        log('imgupload instancied!');
       }
       instance = $(this).data('instance');
     });
-  }
+  };
 
 })(jQuery);
